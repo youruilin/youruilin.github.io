df3ae2f4e493805f557a1533bd7da7ec82abda12
youruilin <youruilin@gmail.com>
2024-09-28 15:30:06 +0800

    Site updated: 2024-09-28 15:30:06

--- js/txmap.js
+++ js/txmap.js
@@ -214,12 +214,32 @@ function showWelcome() {
   else if (date.getHours() >= 19 && date.getHours() < 24) timeChange = "<span>晚上好</span>，夜生活嗨起来！";
   else timeChange = "夜深了，早点休息，少熬夜。";
 
+  // 显示弹框欢迎信息
+  let welcomeInfo = timeChange.replace(/<[^>]*>/g, '');
+  
+
   try {
       //自定义文本和需要放的位置
       document.getElementById("welcome-info").innerHTML =
-          `<b><center>🎉 欢迎信息 🎉</center>&emsp;&emsp;欢迎来自 <span style="color:var(--theme-color)">${pos}</span> 的小伙伴，${timeChange}您现在距离站长约 <span style="color:var(--theme-color)">${dist}</span> 公里，当前的IP地址为： <span style="color:var(--theme-color)">${ip}</span>， ${posdesc}</b>`;
-  } catch (err) {
-      // console.log("Pjax无法获取#welcome-info元素🙄🙄🙄")
+          `<b><center>🎉 欢迎信息 🎉</center>&emsp;&emsp;欢迎来自 <span style="color:var(--theme-color)">${pos}</span> 的小伙伴，${timeChange}您现在距离站长约 <span style="color:var(--theme-color)">${dist}</span> 公里，当前的IP地址为： <span style="color:var(--theme-color)">${ip}</span>， ${posdesc}</b>`;    
+    
+      debounce(function () {
+        new Vue({
+            data: function () {
+            this.$notify({
+                title: "おはよう！😜",
+                message: `${welcomeInfo}`,
+                position: 'top-left',
+                offset: 50,
+                showClose: true,
+                type: "warning",
+                duration: 5000
+            });
+            }
+        })
+        }, 2000);
+    } catch (err) {
+    // console.log("Pjax无法获取#welcome-info元素🙄🙄🙄")
   }
 }
 window.onload = showWelcome;
