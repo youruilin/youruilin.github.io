6bd193a171d6de9dfe4c7b38c04875cdb95340c2
youruilin <youruilin@gmail.com>
2024-12-09 15:33:37 +0000

    新增了日语文章 UTC 2024-12-09 Monday 15:33:37 Updated By Github Actions

---
+++ js/f12.js
@@ -0,0 +1,20 @@
+// f12提醒但不禁用
+document.onkeydown = function (e) {
+  if (123 == e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) {
+    debounce(function () {
+      new Vue({
+        data: function () {
+          this.$notify({
+            title: "你已被发现😜",
+            message: "小伙子，扒源记住要遵循GPL协议！",
+            position: 'top-left',
+            offset: 50,
+            showClose: true,
+            type: "warning",
+            duration: 5000
+          });
+        }
+      })
+    }, 300);
+  }
+};
